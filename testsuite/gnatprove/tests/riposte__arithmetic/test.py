915a405feae2b3bada84894ff3b4cc1317cdfbca
Benedikt Becker <benedikt.becker@inria.fr>
2021-05-25 13:34:41 +0200

    TC02-027 Enable RAC for test riposte__arithmetic
    * testsuite/gnatprove/tests/riposte__arithmetic/test.py:
    Enable RAC and tracing of RAC.
    
    * testsuite/gnatprove/tests/riposte__arithmetic/test.out:
    Update oracle with RAC output.
    
    Change-Id: I093e6e733ec3f3ffef9dd741665091669e41ab3d

--- testsuite/gnatprove/tests/riposte__arithmetic/test.py
+++ testsuite/gnatprove/tests/riposte__arithmetic/test.py
@@ -1,4 +1,8 @@
+import os
 from test_support import *
 
-prove_all ()
+os.environ['GNATPROVE_RAC'] = 'yes'
+os.environ['GNATPROVE_RAC_TRACE'] = 'yes'
+
+prove_all (sort_output=False, check_counterexamples=True)
 check_counterexamples()
